6a950c3ba82f918db80d8404d05dd80f06471856
Mick <suncombo@gmail.com>
2017-04-10 17:28:56 +0800

    [Fix]修改function 'make_po' 覆蓋繼承問題

--- models/supplierinfo.py
+++ models/supplierinfo.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 class SupplierInfo(models.Model):
     _inherit = 'product.supplierinfo'
 
-    location_id = fields.Many2one('stock.location', string="Supply To Location", company_dependent=True,
+    location_id = fields.Many2one('stock.location', string="Supply To Location", company_dependent=True, copy= True,
                                   domain = [('usage','=','internal')])
 
